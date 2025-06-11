9cd8dc42d4c272a8eec81c10eec8f2709cdf6490
Syed Mahbubur Rahman <shakilsyead@gmail.com>
2025-06-11 17:46:22 +0600

    Create LinkedInService.php

---
+++ app/Services/Platforms/LinkedInService.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Services\Platforms;
+
+use App\Enums\PlatformName;
+use App\Interfaces\SocialPlatformServiceInterface;
+use App\Models\SocialConnection;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
+use Exception;
+use Laravel\Socialite\Contracts\User as SocialiteUserContract;
+
+class LinkedInService implements SocialPlatformServiceInterface
+{
+
+}
